1c7e04bf9dd9ddbcc600bf1104053948f52d3f5f
Oscar <morrisonoscar396@gmail.com>
2020-10-29 14:23:03 -0700

    Removed local build and devel files

--- expbot/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/oscarmorrison/Documents/AUVIC/Misc_Env/expdfsa/expbot/src
-# Build directory: /home/oscarmorrison/Documents/AUVIC/Misc_Env/expdfsa/expbot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("fsm")
--- expbot/build/fsm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/oscarmorrison/Documents/AUVIC/Misc_Env/expdfsa/expbot/src/fsm
-# Build directory: /home/oscarmorrison/Documents/AUVIC/Misc_Env/expdfsa/expbot/build/fsm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
