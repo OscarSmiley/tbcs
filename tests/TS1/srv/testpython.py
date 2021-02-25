030702b160da608a58c50492e3d14568b5971470
oscarmorrison <morrisonoscar39@gmail.com>
2021-02-24 16:55:38 -0800

    First commit of fork tbcs of expbot

--- tests/TS1/srv/testpython.py
+++
@@ -1,3 +0,0 @@
-class TESTpy:
-    def __init__(self):
-        print("TESTpy")
--- tests/src/test1.py
+++
@@ -1,8 +0,0 @@
-#!bin/bash/python
-import sys
-import StateMachine
-def main(args):
-    testMachine = StateMachine()
-    testMachine.runStates()
-if __name__ == '__main__':
-    main(sys.argv[1:])
